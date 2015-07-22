959709f8897cce2d08a271d0360b03917924a8d1
JCode89 <JV.Colon@Yahoo.com>
2015-07-22 01:47:41 -0400

    Runs all the different methods.

--- TweetMiner.py
+++ TweetMiner.py
@@ -1,23 +1,20 @@
 # remember in windows, for now, to use chcp 65001
 # type that into the terminal then hit enter then 
 # run your script.
-import sqlite3
+
 from TweetCollector import TwitterAnalyzer, TweetStream
 
 twitter = TwitterAnalyzer()
 streamer = TweetStream()
 
        
-try:
-    conn = sqlite3.connect('TweetData.db')
-    c = conn.cursor()
-    twitter.key_grabber()# loads the keys needed for OAuth
-    twitter.twitter_analytics()
-    twitter.twitter_miner()# requires the calling of twitter.key_grabber() 
-    streamer.key_grabber()
-    streamer.streamer()# A live stream of tweets
+
+   
+  
+twitter.key_grabber()# loads the keys needed for OAuth
+twitter.twitter_analytics()# prints out data from the twitter analytics csv file
+twitter.twitter_miner()# requires the calling of twitter.key_grabber() 
+streamer.key_grabber()
+streamer.streamer()# A live stream of tweets
    
     
-finally:
-    conn.close()
-    print("Connection Terminated")
