180994b5e9b96d172bbcbc44b2488df5484366b4
JCode89 <JV.Colon@Yahoo.com>
2015-07-21 23:13:34 -0400

    Supports live stream of tweets.

--- TweetMiner.py
+++ TweetMiner.py
@@ -1,24 +1,23 @@
-# remember in windows, for now, to use chcp65001
+# remember in windows, for now, to use chcp 65001
 # type that into the terminal then hit enter then 
 # run your script.
 import sqlite3
-import csv
+from TweetCollector import TwitterAnalyzer, TweetStream
 
+twitter = TwitterAnalyzer()
+streamer = TweetStream()
+
+       
 try:
     conn = sqlite3.connect('TweetData.db')
     c = conn.cursor()
-    file_name = "tweet_JvCode.csv"
-    text_data ={}
+    twitter.key_grabber()# loads the keys needed for OAuth
+    twitter.twitter_analytics()
+    twitter.twitter_miner()# requires the calling of twitter.key_grabber() 
+    streamer.key_grabber()
+    streamer.streamer()# A live stream of tweets
+   
     
-    with open(file_name) as f:
-        data = csv.DictReader(f)
-        for row in data:
-            text_data[row['Tweet text']]=row['engagements']
-        max_num = max(text_data.values())# grabs the maximum number of engagements
-        for key, value in text_data.items():# loops through the dictionary
-            if value == max_num:# checks the dictionary value(engagements) against our known max
-                print (key)# prints the key(Tweet_text) out so we know which was most engaging. 
-       
 finally:
     conn.close()
     print("Connection Terminated")
