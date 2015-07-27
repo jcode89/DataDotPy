3ac8947289c76672f3d331209bc009887cdbf5eb
Jcode89 <JV.Colon@yahoo.com>
2015-07-27 01:47:41 -0400

    Writes the results of the analysis to a new table in the DB.

--- TweetWrite.py
+++ TweetWrite.py
@@ -1,7 +1,9 @@
 import sqlite3
+import datetime
 
 conn = sqlite3.connect('TweetData.db')
 c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS data(Total Tweets, Actual User, Tweet User Ratio, Date Time)''')
 fetch = c.execute("select user, text from codenewbie")
 total = 0
 open_file = open("ChatArchive.txt", 'w')
@@ -15,11 +17,18 @@ for user, text in fetch:# unpack the tuple
     total+=1
 for text in chat_list:
     open_file.write((str(text)+ "\n"+"\n"))# write each line in the list to the file using a double space for each.
+user_num = len(actual_user)
+user_ratio = float(total/len(actual_user))
+time_now = datetime.datetime.now().strftime("%B, %d, %Y, %X")
 print("Number of Tweets: %d" % total)
 # Number of users that sent out tweets.
 # Multiple tweets per user still count as one user.
-print("Number of actual users: %d" % len(actual_user))
+print("Number of actual users: %d" % user_num)
 # Tweets per user, or Tweets:user
-print("Tweets per user: %.2f:1" % (total/len(actual_user)))
+print("Tweets per user: %.2f" % user_ratio)
+# Prints exact time programs was run, including the date.
+print(time_now)
+c.execute("INSERT INTO data VALUES(?,?,?,?)", (total, user_num, user_ratio, time_now))
+conn.commit()
 open_file.close()
 conn.close()
