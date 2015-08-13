cc5e8aa6bc8c1091de7ab6c23b414560d16b803f
jcode89 <jv.colon@yahoo.com>
2015-08-13 17:57:53 -0400

    Adds functionality to choose what you would like to do.

--- RethinkDB Version/TweetMiner.py
+++ RethinkDB Version/TweetMiner.py
@@ -1,20 +1,31 @@
 # remember in windows, for now, to use chcp 65001
-# type that into the terminal then hit enter then 
+# type that into the terminal then hit enter then
 # run your script.
 
-from TweetCollector import TwitterAnalyzer, TweetStream
+from TweetCollector import TweetStream, database_connect
+from TweetSifter import TwitterAnalyzer
 
+# Create an instance of the class you want to use
 twitter = TwitterAnalyzer()
 streamer = TweetStream()
 
-       
-
-   
-  
-twitter.key_grabber()# loads the keys needed for OAuth
-twitter.twitter_analytics()# prints out data from the twitter analytics csv file
-twitter.twitter_miner()# requires the calling of twitter.key_grabber() 
+# Load the keys needed for OAuth
+twitter.key_grabber()
 streamer.key_grabber()
-streamer.streamer()# A live stream of tweets
-   
-    
+
+# Connect and create your database and table
+connect = database_connect()
+
+print('''What would you like to do open a stream,
+    work with the RESTful, or with the csv file?''')
+response = input("> ").lower()
+
+if response == "restful":
+    twitter.twitter_miner()# requires the calling of twitter.key_grabber()
+elif response == "csv":
+    twitter.twitter_analytics()# prints out data from the twitter analytics csv file
+elif response == "stream":
+    streamer.streamer()# A live stream of tweets
+else:
+    print("Oops, check your spelling!")
+
