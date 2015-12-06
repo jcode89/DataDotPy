0196876d28615f3c96bfc341031bf1ee5e0a7031
Jamal Hansen <jamal.hansen@gmail.com>
2015-12-06 16:14:40 -0600

    Fixes style issues

--- data_py/configuration.py
+++ data_py/configuration.py
@@ -4,9 +4,10 @@ This module contains all of the configuration tools and informaton
 import os
 
 def environ(key):
+    """Wrapper around Python's os.environ"""
     return os.environ[key]
 
-class Configuration:
+class Configuration(object):
     """A central structure for holding config items"""
     def __init__(self):
         self.consumer_key = ""
@@ -14,20 +15,21 @@ class Configuration:
         self.access_token = ""
         self.access_token_secret = ""
 
-class EnvironmentConfiguration:
+class EnvironmentConfiguration(object):
     """
     A class that will populate your configuration from environment variables
     """
-    def __init__(self, consumer_key_name = "TWITTER_API_KEY", \
-                      consumer_secret_name="TWITTER_API_SECRET", \
-                      access_token_name = "TWITTER_ACCESS_TOKEN", \
-                      access_token_secret_name = "TWITTER_ACCESS_TOKEN_SECRET"):
+    def __init__(self, consumer_key_name="TWITTER_API_KEY", \
+                       consumer_secret_name="TWITTER_API_SECRET", \
+                       access_token_name="TWITTER_ACCESS_TOKEN", \
+                       access_token_secret_name="TWITTER_ACCESS_TOKEN_SECRET"):
         self.consumer_key_name = consumer_key_name
         self.consumer_secret_name = consumer_secret_name
         self.access_token_name = access_token_name
         self.access_token_secret_name = access_token_secret_name
 
     def build_configuration(self):
+        """Returns a populated configuration instance"""
         conf = Configuration()
         conf.consumer_key = environ(self.consumer_key_name)
         conf.consumer_secret = environ(self.consumer_secret_name)
